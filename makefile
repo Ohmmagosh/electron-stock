4a98d421472ae94d42b837eb5bee74a17594d2f5
ohmmaagoch <ohm@42bangkok.com>
2023-11-09 02:44:05 +0700

    test lazygit
    lazygit

---
+++ makefile
@@ -0,0 +1,4 @@
+run:
+	cd src && npm start
+
+.PHONY: run
--- src/main.js
+++ src/main.js
@@ -1,14 +1,23 @@
 const { app, BrowserWindow } = require('electron')
+const path = require('path')
 
 const createWindow = () => {
 	const win = new BrowserWindow({
+		title: 'Stocks',
 		width: 800,
 		height: 600
 	})
 
-	win.loadFile('index.html')
+	win.loadFile(path.join(__dirname,'./renderer/index.html'));
 }
 
 app.whenReady().then(() => {
 	createWindow()
-})
+	app.on('activate', () => {
+		if (BrowserWindow.getAllWindows().length === 0) {
+			createWindow()
+		}
+	});
+	console.log('hello')
+});
+
